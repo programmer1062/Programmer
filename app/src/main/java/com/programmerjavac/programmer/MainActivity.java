88b32f9db58f83f237ed26c5f393efce045908e2
programmer1062 <35653248+programmer1062@users.noreply.github.com>
2018-12-10 21:52:28 +0530

    update main activity.java

--- app/src/main/java/com/programmerjavac/programmer/MainActivity.java
+++ app/src/main/java/com/programmerjavac/programmer/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },1000);
 
     }
 }
